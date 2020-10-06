15c70488a06413f7180ad23642b2a1c5f86e7edc
Tyler Hipke <tylerhipke@gmail.com>
2020-10-05 22:40:31 -0700

    add js dynamic card height

---
+++ assets/scripts/app.js
@@ -0,0 +1,12 @@
+// portfolio app script
+
+
+// get card width
+var element = document.getElementById('leadingcard');
+var positionInfo = element.getBoundingClientRect();
+var cardWidth = positionInfo.width + "px";
+
+var cardWidth 
+
+//set card height to width
+document.getElementById("leadingcard").style["height"] = cardWidth ;
