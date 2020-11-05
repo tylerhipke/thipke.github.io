af2544f5282198a43554461bfaf4b1c5e7adbd43
Tyler Hipke <tylerhipke@gmail.com>
2020-11-05 00:24:09 -0800

    begin block renderer

--- public/assets/scripts/webapp.js
+++ public/assets/scripts/webapp.js
@@ -1,5 +1,26 @@
 //webapp handlers
 
+var articles = {
+    0: {
+        0: {
+            type: "header",
+            content: ["Nicole Frank TWE"]
+        },
+        1: {
+            type: "subheader",
+            content: ["full-stack shopify project"]
+        },
+        2: {
+            type: "text",
+            content: ["nicole frank twe here shows the different options blah blah"]
+        },
+        3: {
+            type: "image",
+            content: ["./assets/graphics/blackbox.png", "image text here"]
+        }
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     readLink();
 });
@@ -20,7 +41,8 @@ function readLink() {
     $('#gohome').click(function () {
         exitArticle();
     })
-}
+    renderArticle(0);
+};
 
 function enterArticle(type) {
     $('#homeblock').removeClass('t-visible').addClass('t-hidden');
@@ -32,7 +54,7 @@ function enterArticle(type) {
     setTimeout(function () {
         $('#articleblock').removeClass('t-hidden').addClass('t-visible');
     }, 400);
-}
+};
 
 function exitArticle(type) {
     $('#articleblock').removeClass('t-visible').addClass('t-hidden');
@@ -44,4 +66,41 @@ function exitArticle(type) {
     setTimeout(function () {
         $('#homeblock').removeClass('t-hidden').addClass('t-visible');
     }, 400);
+};
+
+function renderArticle(id) {
+    //pull in proper article via id 
+    //begin for loop that reads the article and processes information
+    var thisArticle = articles[id];
+    console.log("render Article triggered on id " + id);
+
+    var articleKeys = Object.keys(articles[id]);
+    var articleLength = articleKeys.length;
+    console.log(articleLength);
+
+    for (i = 0; i < articleLength; i++) {
+        console.log(thisArticle[i]);
+        var blockType = thisArticle[i].type;
+        console.log(blockType);
+    }
+};
+
+function renderHeaderBlock(){
+
+}
+
+function renderSubheaderBlock(){
+
+}
+
+function renderImageBlock(){
+
+}
+
+function renderTextBlock(){
+
+}
+
+function renderGalleryBlock(){
+
 }
